01c757869098db9dbf496d48a970d129ccde93b2
bakuzan <imannoyed4days@hotmail.co.uk>
2018-04-11 16:11:43 +0100

    playing around with settings. understanding css modules.

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,43 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import scss from 'rollup-plugin-scss';
 import commonjs from 'rollup-plugin-commonjs';
+import autoprefixer from 'autoprefixer'
 import postcss from 'rollup-plugin-postcss';
 import postcssModules from 'postcss-modules';
 
+const cssExportMap = {};
 export default {
   input: 'lib/index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs'
   },
-  // All the used libs needs to be here
   external: [
     'react'
   ],
   plugins: [
     resolve(),
-    scss({
-      output: true
-    }),
     postcss({
+      preprocessor: (content, id) => new Promise((resolve, reject) => {
+        const result = scss.renderSync({ file: id })
+        resolve({ code: result.css.toString() })
+      }),
+      plugins: [
+        autoprefixer,
+        postcssModules({
+          getJSON (id, exportTokens) {
+            cssExportMap[id] = exportTokens;
+          }
+        })
+      ],
       modules: true,
       minimize: false,
-      extract: 'dist/styles.css',
+      getExportNamed: false,
+      getExport(id) {
+        return cssExportMap[id];
+      },
+      extract: 'dist/bundle.css',
+      extensions: ['.scss']
     }),
     babel({
       exclude: 'node_modules/**'
