f60c9fbf9d2a49f559a30ee190cfcd8f13689f29
bakuzan <imannoyed4days@hotmail.co.uk>
2018-04-12 09:21:54 +0100

    storybook now loaders scss modules!

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,8 +5,37 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "../")
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]__[local]___[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: loader => [
+                require("autoprefixer")()
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]__[local]___[hash:base64:5]"
+            }
+          },
+        ],
+        include: path.resolve(__dirname, "../lib")
       }
     ]
   }
