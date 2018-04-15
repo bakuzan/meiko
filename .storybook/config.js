4f1154f7386c024b46d16d96a133f52fb325f270
bakuzan <imannoyed4days@hotmail.co.uk>
2018-04-15 22:00:09 +0100

    connected scss modules. writing stories.

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,8 +4,9 @@ function loadStories() {
   require('../lib/stories/index.js');
   require('../lib/stories/alert.js');
   require('../lib/stories/autocomplete-input.js');
-
+  require('../lib/stories/chip-list-input.js');
   require('../lib/stories/clearable-input.js');
+  require('../lib/stories/header.js');
   require('../lib/stories/loaders.js');
 }
 
