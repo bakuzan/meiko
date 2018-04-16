7565a1653991e861281e6af1787a7abb436031a3
bakuzan <imannoyed4days@hotmail.co.uk>
2018-04-16 15:00:14 +0100

    working on malsearch story.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,19 +1,9 @@
 import { configure } from '@storybook/react';
 
+const req = require.context('../lib/stories', true, /\.js$/);
+
 function loadStories() {
-  require('../lib/stories/index.js');
-  require('../lib/stories/alert.js');
-  require('../lib/stories/autocomplete-input.js');
-  require('../lib/stories/chip-list-input.js');
-  require('../lib/stories/clearable-input.js');
-  require('../lib/stories/header.js');
-  require('../lib/stories/loaders.js');
-  require('../lib/stories/radio-button.js');
-  require('../lib/stories/rating-control.js');
-  require('../lib/stories/select-box.js');
-  require('../lib/stories/svg-logo.js');
-  require('../lib/stories/tabs.js');
-  require('../lib/stories/tickbox.js');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
