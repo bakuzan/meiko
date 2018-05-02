c2bd62969792167b94d4da80ac89849484f9edc7
bakuzan <imannoyed4days@hotmail.co.uk>
2018-05-02 22:03:32 +0100

    changes as previous sass modules didnt work at all.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,12 @@
-import fs from 'fs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import scss from 'rollup-plugin-scss';
+import sassModules from 'rollup-plugin-sass-modules';
 import commonjs from 'rollup-plugin-commonjs';
 import autoprefixer from 'autoprefixer'
-import postcss from 'rollup-plugin-postcss';
-import postcssModules from 'postcss-modules';
+import postcss from 'postcss';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 import includePaths from 'rollup-plugin-includepaths';
-import CssModulesSassLoader from './css-modules-loader';
 import pkg from './package.json';
 
 const entry = 'lib/index.js';
@@ -57,35 +54,34 @@ function rollupPlugins() {
       browser: true,
       preferBuiltins: false
     }),
-    postcss({
-      preprocessor: (content, id) => new Promise((resolve, reject) => {
-        const result = scss.renderSync({ file: id });
-        resolve({ code: result.css.toString() });
-      }),
-      plugins: [
-        autoprefixer,
-        postcssModules({
-          Loader: CssModulesSassLoader, // Load all "composes" files with Sass
-          generateScopedName: '[name]__[local]___[hash:base64:5]',//'[hash:base64:5]',
-          getJSON: function(cssFileName, json, outputFileName) {
-            const path          = require('path');
-            const cssName       = path.basename(cssFileName, '.css');
-            const jsonFileName  = path.resolve('./dist/json/' + cssName + '.json');
-            fs.writeFileSync(jsonFileName, JSON.stringify(json));
-          },
-        })
-      ],
-      minimize: true,
-      sourceMaps: false,
-      modules: true,
-      extract: 'dist/bundle.css',
-      extensions: ['.scss']
+    sassModules({
+      include: ['lib/**/*.scss'],
+      options: {
+        output: 'dist/bundle.css',
+        minimize: false,
+        sourceMaps: false,
+      },
+      processor: css => postcss([autoprefixer])
+      .process(css, { from: undefined })
+      .then(result => result.css)
     }),
     babel({
       exclude: 'node_modules/**',
       plugins: ["external-helpers"]
     }),
-    commonjs(),
-    uglify()
+    commonjs()
   ]
 }
+
+/*
+            postcssModules({
+              Loader: CssModulesSassLoader, // Load all "composes" files with Sass
+              generateScopedName: '[name]__[local]___[hash:base64:5]',//'[hash:base64:5]',
+              getJSON: function(cssFileName, json, outputFileName) {
+                const path          = require('path');
+                const cssName       = path.basename(cssFileName, '.css');
+                const jsonFileName  = path.resolve('./dist/json/' + cssName + '.json');
+                fs.writeFileSync(jsonFileName, JSON.stringify(json));
+              },
+            })
+ */
