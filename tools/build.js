a3a9eb6b76734be5ba852c6f63d8f000ed96b0d5
bakuzan <imannoyed4days@hotmail.co.uk>
2018-11-28 10:06:29 +0000

    ready to work on moving files.

--- tools/build.js
+++ tools/build.js
@@ -11,27 +11,39 @@ const libRoot = path.join(__dirname, '../lib/');
 
 const clean = async (dir) => fse.existsSync(dir) && fse.remove(dir);
 
-const step = (name, root, fn) => async () => {
+const step = (name, fn) => async () => {
   console.log(cyan('Building: ') + green(name));
-  await clean(root);
   await fn();
   console.log(magenta('Built: ') + green(name));
 };
 
-const buildLib = step('commonjs modules', outputRoot, () =>
-  execa.shell(`npx babel ${libRoot} --out-dir ${outputRoot} --env-name "lib"`, {
+const buildLib = step('commonjs modules', () =>
+  execa.shell(`npx babel ${outputRoot} --env-name "lib"`, {
     stdio
   })
 );
 
+const copyFiles = async () => {
+  console.log(cyan('Copying files'));
+  const [src, dest] = [libRoot, outputRoot];
+  try {
+    await fse.copy(src, dest);
+    console.log(magenta('Copied: ') + green(`${src} => ${dest}`));
+  } catch (err) {
+    console.error(red(err));
+  }
+};
+
 console.log(green('Building library\n'));
 
-Promise.resolve(buildLib()).catch((err) => {
-  if (err) {
-    console.error(red(err.stack || err.toString()));
-  } else {
-    console.log(green('Built library\n'));
-  }
+clean(outputRoot)
+  .then(copyFiles)
+  .then(buildLib)
+  .then(() => console.log(green('Built library\n')))
+  .catch((err) => {
+    if (err) {
+      console.error(red(err.stack || err.toString()));
+    }
 
-  process.exit(1);
-});
+    process.exit(1);
+  });
