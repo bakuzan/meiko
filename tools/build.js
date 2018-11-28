3e2b130d7d1c45468d481c21e624c46d5fd6aafb
bakuzan <imannoyed4days@hotmail.co.uk>
2018-11-28 10:17:38 +0000

    working module script..

--- tools/build.js
+++ tools/build.js
@@ -13,32 +13,23 @@ const clean = async (dir) => fse.existsSync(dir) && fse.remove(dir);
 
 const step = (name, fn) => async () => {
   console.log(cyan('Building: ') + green(name));
+  await clean(outputRoot);
   await fn();
   console.log(magenta('Built: ') + green(name));
 };
 
 const buildLib = step('commonjs modules', () =>
-  execa.shell(`npx babel ${outputRoot} --env-name "lib"`, {
-    stdio
-  })
+  execa.shell(
+    `npx babel ${libRoot} --out-dir ${outputRoot} --copy-files --env-name "lib"`,
+    {
+      stdio
+    }
+  )
 );
-
-const copyFiles = async () => {
-  console.log(cyan('Copying files'));
-  const [src, dest] = [libRoot, outputRoot];
-  try {
-    await fse.copy(src, dest);
-    console.log(magenta('Copied: ') + green(`${src} => ${dest}`));
-  } catch (err) {
-    console.error(red(err));
-  }
-};
 
 console.log(green('Building library\n'));
 
-clean(outputRoot)
-  .then(copyFiles)
-  .then(buildLib)
+buildLib()
   .then(() => console.log(green('Built library\n')))
   .catch((err) => {
     if (err) {
