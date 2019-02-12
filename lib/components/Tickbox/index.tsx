ced97da942c0f1ba1543475ce86de18d7e43a67e
bakuzan <imannoyed4days@hotmail.co.uk>
2019-02-12 16:02:29 +0000

    converted more.

--- lib/components/Tickbox/index.tsx
+++ lib/components/Tickbox/index.tsx
@@ -1,7 +1,10 @@
 import classNames from 'classnames';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-import './Tickbox.scss';
+
+import styled from '../../styles';
+import { StyledControlContainer } from '../../styles/generic';
+import { zIndexes } from '../../styles/variables';
 
 export interface ITickboxProps {
   className?: string;
@@ -12,6 +15,52 @@ export interface ITickboxProps {
   onChange(e: React.ChangeEvent<HTMLInputElement>): void;
 }
 
+const StyledCheckbox = styled.label<{ disabled: boolean }>`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 2px;
+  cursor: pointer;
+
+  input[type='checkbox'] {
+    appearance: none;
+    transition: all 0.3s;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20px;
+    height: 20px;
+    margin: 0 5px;
+  }
+
+  ${(props) => props.disabled && 'cursor: default;'}
+
+  input:before,
+  input:checked:before {
+    color: #000;
+    transition: all 0.3s;
+    cursor: pointer;
+    z-index: ${zIndexes.get('wafer')};
+  }
+
+  input:before {
+    content: '\2610';
+    font-size: 2em;
+  }
+
+  input:disabled:before {
+    content: '\274c';
+    color: #666;
+    cursor: default;
+  }
+
+  input:checked:before {
+    content: '\2611';
+    color: #0f0;
+  }
+`;
+
 const Tickbox = ({
   className,
   name,
@@ -20,10 +69,11 @@ const Tickbox = ({
   onChange,
   text
 }: ITickboxProps) => (
-  <div className={classNames('input-container', className)}>
-    <label
+  <StyledControlContainer className={classNames('input-container', className)}>
+    <StyledCheckbox
       className={classNames('tickbox', { 'tickbox--disabled': disabled })}
       htmlFor={name}
+      disabled={disabled}
     >
       <input
         type="checkbox"
@@ -34,8 +84,8 @@ const Tickbox = ({
         onChange={onChange}
       />
       {text || ''}
-    </label>
-  </div>
+    </StyledCheckbox>
+  </StyledControlContainer>
 );
 
 Tickbox.propTypes = {
