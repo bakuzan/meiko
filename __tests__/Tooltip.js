bad2ed3875b6516c6750232086c2906990fefa5b
bakuzan <imannoyed4days@hotmail.co.uk>
2019-09-14 20:57:47 +0100

    added skipped test.

--- __tests__/Tooltip.js
+++ __tests__/Tooltip.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Tooltip } from '../lib';
+import { act } from 'react-testing-library';
 
 it('should render with minimum props', function() {
   const component = shallow(<Tooltip text="Tooltip!" />);
@@ -9,5 +10,29 @@ it('should render with minimum props', function() {
   expect(component).toMatchSnapshot();
 });
 
-// TODO
-// Add more tests
+xit('should add/remove hover class on mouseenter/mouseleave', function() {
+  const component = mount(<Tooltip text="Tooltip!" />);
+
+  let classNameIncludesHover = component
+    .find('.tooltip__content')
+    .prop('className')
+    .includes('tooltipContent__hovered');
+
+  expect(classNameIncludesHover).toBe(false);
+
+  act(() => {
+    component.find('.tooltip').prop('onMouseEnter')({
+      clientX: 10,
+      clientY: 10
+    });
+  });
+
+  classNameIncludesHover = component
+    .find('.tooltip__content')
+    .prop('className')
+    .includes('tooltipContent__hovered');
+
+  expect(classNameIncludesHover).toBe(true);
+
+  expect(component).toMatchSnapshot();
+});
