6f5071a3f51243687660d33753674de51de7b9f3
bakuzan <imannoyed4days@hotmail.co.uk>
2019-09-27 09:16:55 +0100

    Improving image src handling.

--- stories/image.js
+++ stories/image.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { MkoContext } from 'mko-book';
 import Image from '@/Image';
 
 const offPageStyle = {
@@ -9,7 +10,47 @@ const offPageStyle = {
   height: '1500px'
 };
 
+function ImageStory() {
+  const inp = useRef();
+  const [state, setState] = useContext(MkoContext);
+
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          minWidth: 300,
+          margin: 10
+        }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <label htmlFor="imageUrl">Image url</label>
+          <input ref={inp} id="imageUrl" type="url" />
+        </div>
+        <button
+          type="button"
+          onClick={() => setState({ value: inp.current.value })}
+        >
+          Update
+        </button>
+      </div>
+      <Image src={state.value} alt="changing source" />
+      <div style={offPageStyle}>
+        <Image src={state.value} alt="changing source lazy" isLazy />
+      </div>
+    </div>
+  );
+}
+
 storiesOf('Image', module)
+  .addParameters({
+    mko: { defaultValues: { value: '' } },
+    props: {
+      propTables: [Image],
+      propTablesExclude: [ImageStory]
+    }
+  })
   .add('load image', () => (
     <Image src="https://i.imgur.com/5hLv07N.jpg" alt="loaded" />
   ))
@@ -20,4 +61,5 @@ storiesOf('Image', module)
   ))
   .add('on load error', () => (
     <Image src="https://bad-url/will-error" alt="error test" />
-  ));
+  ))
+  .add('on new src', () => <ImageStory />);
