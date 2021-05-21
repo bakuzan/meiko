c81f82fe0cee2cc1b2d694f3cd49a4368ea0d30f
bakuzan <imannoyed4days@hotmail.co.uk>
2021-05-21 13:46:08 +0100

    Type adjustments

--- types/components/Grid.d.ts
+++ types/components/Grid.d.ts
@@ -1,17 +1,17 @@
 import * as React from 'react';
 
-export interface GridProps<T extends any>
-  extends React.HTMLProps<HTMLUListElement> {
+export interface GridProps<T extends any> extends React.HTMLProps<HTMLElement> {
   items?: T[];
   noItemsText?: string | boolean;
   children?: (data: T, index: number, arr: T[]) => React.ReactNode;
   uniformRows?: boolean;
   footerChildren?: React.ReactNode | React.ReactElement<any>;
+  element?: keyof JSX.IntrinsicElements;
 }
 
 declare const Grid: <T extends any>(
   props: React.PropsWithChildren<GridProps<T>>,
-  ref: React.Ref<HTMLUListElement>
+  ref: React.Ref<HTMLElement>
 ) => React.ReactElement | null;
 
 export default Grid;
